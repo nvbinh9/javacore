f562d82f53abe2dbb137bcb2fdb24e78ce8c3bde
nvbinh9 <sang2k1234@gmail.com>
2022-01-20 09:43:40 +0700

    commited javadev

--- src/JavaDev.java
+++ src/JavaDev.java
@@ -1,5 +1,8 @@
 public class JavaDev {
     public static void main(String[] args) {
         System.out.println("nvbinh");
+        int a = 4;
+        int b = 5;
+        System.out.println(a + b);
     }
 }
